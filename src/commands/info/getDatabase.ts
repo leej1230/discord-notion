9bd7b987b6c04d00e92c6ccccef8d1e4059dbbbd
leej1230 <leejaewoo1230@gmail.com>
2023-04-02 00:41:32 -0700

    bot now eligible to change the time based on user input, and can fetch the tasks based on that time

--- src/commands/info/getDatabase.ts
+++ src/commands/info/getDatabase.ts
@@ -2,28 +2,49 @@ import { Client } from "@notionhq/client";
 import { Command } from "../../structures/Commands";
 
 const notion = new Client({auth: process.env['notionIntegrationToken']});
-
 const databaseId:string = process.env['notionTodayTaskDatabaseId']!;
 
 export default new Command({
     name: 'getdatabase',
     description: 'Retrieves database from notion database',
     run: async({ interaction }) => {
-        var today = new Date();
+        const fs = require("fs");
+        let d = fs.readFileSync("user.json") // reads the json file
+        let users = JSON.parse(d) // turns json into js
+        var h:number = users["timeDifferenceBetweenUTC"];
+
+        var today:Date = new Date();
+        today.setHours( today.getHours() + h );
         var todayInISO = today.toISOString();
-        interaction.followUp(databaseId);
+        interaction.followUp(todayInISO);
         const data = await notion.databases.query({
             database_id:databaseId,
             filter: {
-                // and: [
-                    // {
+                and: [
+                    {
                         property: "Due Date",
                         date: {
                             equals: todayInISO.slice(0,10),
                         },
-                    // },
+                    },
+                    {
+                        or: [
+                            {
+                            property: "Status",
+                            status: {
+                                equals: "To Do",
+                            }
+                        },
+                            {
+                            property: "Status",
+                            status: {
+                                equals: "In progress",
+                            }
+                        },
+                        ]
+                    },
 
-                // ]
+                ]
             }
         });
         console.log(data);
--- src/commands/setInfo/changeTime.ts
+++ src/commands/setInfo/changeTime.ts
@@ -1,9 +1,12 @@
 import { ApplicationCommandOptionType } from "discord.js";
 import { Command } from "../../structures/Commands";
 
+var today:Date = new Date();
+var todayInISO = today.toISOString();
+
 export default new Command({
     name: 'changetime',
-    description: 'Input the number 1 to 23 to adjust the time of the bot chilling (not yet implemented)',
+    description: `The hour will be added or subtracted from UTC(${todayInISO})`,
     options: [
         {
             name: 'hour',
@@ -13,8 +16,20 @@ export default new Command({
         },
     ],
     run: async({ interaction }) => {
-        const hour = interaction.options.get('hour')?.value
+        const fs = require("fs");
+        let data = fs.readFileSync("user.json") // reads the json file
+        let users = JSON.parse(data) // turns json into js
+
+        const hour:number = Number(interaction.options.get('hour')?.value)
         console.log(interaction.options.get);
-        interaction.followUp(`Your input is ${hour}`);
+        try{
+            today.setHours(today.getHours() + hour);
+            interaction.followUp(`Time is now set to ${today.toISOString()}`);
+            users["timeDifferenceBetweenUTC"] = hour;
+            fs.writeFileSync("user.json", JSON.stringify(users));
+        } catch(e) {
+            interaction.followUp("Invalid Input >:(");
+        }
+        
     }
 });
